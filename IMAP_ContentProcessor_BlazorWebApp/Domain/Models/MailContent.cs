61f9cbdceb717f4f06d83aaf89ea0abad7f19e1e
Michal Kalinowski <kalinowskimichal638@gmail.com>
2025-07-10 21:07:07 +0200

    Add project files.

---
+++ IMAP_ContentProcessor_BlazorWebApp/Domain/Models/MailContent.cs
@@ -0,0 +1,12 @@
+﻿namespace IMAP_ContentProcessor_BlazorWebApp.Domain.Models
+{
+    public class MailContent
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Body { get; set; }
+        public DateTime MailDateTime { get; set; }
+        public List<string> Attachments { get; set; }
+        public string MsgID { get; set; }
+    }
+}
---
+++ IMAP_ContentProcessor_BlazorWebApp/Program.cs
@@ -0,0 +1,61 @@
+using Google.Apis.Gmail.v1;
+using IMAP_ContentProcessor_BlazorWebApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+const string AuthScheme = "IMAP_ContentProcessor_BlazorWebApp";
+
+//builder.Services.AddCascadingAuthenticationState();
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = AuthScheme;
+    options.DefaultChallengeScheme = Google.Apis.Auth.AspNetCore3.GoogleOpenIdConnectDefaults.AuthenticationScheme;
+})
+    //.AddCookie(AuthScheme)
+    .AddCookie(AuthScheme)
+    .AddGoogle(options =>
+    {
+        options.ClientId = builder.Configuration["Authentication:Google:ClientId"]!;
+        options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"]!;
+        options.SignInScheme = AuthScheme;
+        options.AccessType = "offline";
+        options.AccessDeniedPath = "/";
+        options.Scope.Add(Google.Apis.Gmail.v1.GmailService.Scope.GmailCompose);
+        options.Scope.Add(GmailService.Scope.GmailModify);
+        options.Scope.Add(GmailService.Scope.GmailReadonly);
+        options.SaveTokens = true;
+    });
+
+builder.Services.AddAuthorization();
+builder.Services.AddHttpContextAccessor();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication()
+    .UseAuthorization();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
